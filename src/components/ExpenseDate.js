036b775f94af13822ba4e689d14518cbc34003f2
Yusuf <yusufmohd72@gmail.com>
2021-11-01 10:55:03 +0530

    Assignment 1

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css';
+
+const ExpenseDate = ({ date }) => {
+  const month = date.toLocaleString('en-us', { month: 'long' });
+  const day = date.toLocaleString('en-us', { day: '2-digit' });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ title, amount, date }) => {
-  const month = date.toLocaleString('en-us', { month: 'long' });
-  const day = date.toLocaleString('en-us', { day: '2-digit' });
-  const year = date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
