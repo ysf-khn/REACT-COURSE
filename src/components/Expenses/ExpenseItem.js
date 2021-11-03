fdbf3382e9692cb1ca60ff65b3739a70fdbffa5e
Yusuf <yusufmohd72@gmail.com>
2021-11-03 20:44:13 +0530

    Module 4 wrap-up

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,15 @@
-import { useState } from 'react';
-
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ title, amount, date }) => {
-  const [titl, setTitl] = useState(title);
-
-  const clickHandler = () => {
-    setTitl('Neww');
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{titl}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
