795145d273ed6db85084b7b280de4d73f9ffc653
Yusuf <yusufmohd72@gmail.com>
2021-11-04 08:26:13 +0530

    keys prop

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,6 +21,7 @@ const Expenses = ({ items }) => {
         />
         {items.map(expense => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
